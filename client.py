3aaf8c36838118ef4872afd3abd80ea17b80b695
Nongoogle <essamsleem9@gmail.com>
2026-05-05 16:27:10 +0300

    ser connection

--- client.py
+++ client.py
@@ -4,7 +4,7 @@ import tkinter as tk
 from tkinter import filedialog
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect(('server.up.railway.app', 50505))
+client.connect(('chat.up.railway.app', 50505))
 
 def receive():
     buffer = b""
